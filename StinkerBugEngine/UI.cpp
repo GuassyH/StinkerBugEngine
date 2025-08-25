b118a452dc3ecf06e07c06e217f8b317c986ac2f
Guassy <haaglgw@gmail.com>
2025-08-25 21:45:03 +0200

    You can now select an entity, i should make it so all entities inherit from a ComponentClass and have a "Render ImGui" func so i can define how they should be rendered and i can easily render the components

--- StinkerBugEngine/UI.cpp
+++ StinkerBugEngine/UI.cpp
@@ -19,6 +19,7 @@ void UI::imgui_init() {
 	std::cout << "ImGui / UI initialized\n";
 }
 
+uint32_t selected_entity = 0;
 void UI::imgui_render(CameraMovement& camera_move, Scene& scene) {
 
 	ImGui_ImplOpenGL3_NewFrame();
@@ -31,20 +32,33 @@ void UI::imgui_render(CameraMovement& camera_move, Scene& scene) {
 	if (camera_move.focusMouse) { ImGui::SetMouseCursor(ImGuiMouseCursor_None); glfwSetInputMode(display.window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN); }
 	else { ImGui::SetMouseCursor(ImGuiMouseCursor_Arrow); glfwSetInputMode(display.window, GLFW_CURSOR, GLFW_CURSOR_NORMAL); }
 
-
 	if (!camera_move.focusMouse) {
 		ImGui::SetNextWindowPos(ImVec2(10, 10));
 		ImGui::SetNextWindowSize(ImVec2(350, 1060));
-		ImGui::Begin("Debug Menu", &opened, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
+		ImGui::Begin("Hierarchy Menu", &opened, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
 		
 		ImGui::DragFloat("Gravity", &scene.gravity, 0.1f, -50.0f, 50.0f);
 		ImGui::Text("Entities");
-		for (size_t i = 0; i < scene.Scene_ECS.entities.size(); i++)
+		for (uint32_t i = 0; i < scene.Scene_ECS.entities.size(); i++)
 		{
 			std::ostringstream ss; ss << scene.Scene_ECS.entity_names[i];
-			ImGui::DragFloat3(ss.str().c_str(), &scene.Scene_ECS.transforms[i].position.x, 0.1f, -1000, 1000);
+			if(ImGui::Button(ss.str().c_str(), ImVec2(330, 20))) {
+				selected_entity = i;
+				std::cout << "Selected entity " << selected_entity << "\n";
+			}
 		}
 	
+		ImGui::End();
+
+
+		ImGui::SetNextWindowPos(ImVec2(display.windowWidth - 360, 10));
+		ImGui::SetNextWindowSize(ImVec2(350, 1060));
+		ImGui::Begin("GameObject Inspector", &opened, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
+
+		std::ostringstream ss; ss << scene.Scene_ECS.entity_names[selected_entity];
+		ImGui::Text(ss.str().c_str());
+		ImGui::DragFloat3("Transform", &scene.Scene_ECS.transforms[selected_entity].position.x, 0.1f);
+
 		ImGui::End();
 	}
 
